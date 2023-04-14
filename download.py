3be489c6851cde5a73a15636578fe1e8b9c7821d
johnsnack <128845239+johnsnack@users.noreply.github.com>
2023-04-14 21:28:53 +0300

    Update download.py

--- download.py
+++ download.py
@@ -7,9 +7,9 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    model_name = "google/flan-t5-xl"
+    model_name = "google/flan-ul2"
     tokenizer = T5Tokenizer.from_pretrained(model_name)
-    model = T5ForConditionalGeneration.from_pretrained(model_name)
+    model = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto", load_in_8bit=True)
 
 if __name__ == "__main__":
     download_model()
